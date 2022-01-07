70061400beff49fc9af1f72d71baf6674976d6af
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-01-07 00:10:45 -0300

    add cursor menu e news

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useHistory } from 'react-router-dom'
 
 import Logo from "../../assets/M.png";
 import "./styles.css";
 
 const Header = () => {
+  const { push } = useHistory()
+
+  const goToHome = () => push('/')
   
   return (
     <div className="header">
-      <div className="headerLogo">
+      <div className="headerLogo" onClick={() => goToHome()}>
         <img id="logo" src={Logo} alt="Logo Meta Team" />
         <p id="menuTitle">eta Team</p>
       </div>
